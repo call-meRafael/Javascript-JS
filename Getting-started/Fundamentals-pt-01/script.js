843ef94fbb8242b554098a4c97541fde51e0f38d
countdown-br <raaphael.araujo@gmail.com>
2025-10-17 14:41:20 -0300

    advancing in the lectures

--- Getting-started/Fundamentals-pt-01/script.js
+++ Getting-started/Fundamentals-pt-01/script.js
@@ -1,8 +1,14 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-let firstName = "Rafael";
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Rafael";
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// firstName = 'Rafael';
+// console.log(firstName);
+// if (firstName === 'Rafael') {
+//     console.log('Rafael is a good name!');
+// }
 
