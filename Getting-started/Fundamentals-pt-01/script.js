19924c453e1b1cce91393104fe5265d93fa8f002
countdown-br <raaphael.araujo@gmail.com>
2025-10-20 14:21:53 -0300

    remembering basic concepts pt-1

--- Getting-started/Fundamentals-pt-01/script.js
+++ Getting-started/Fundamentals-pt-01/script.js
@@ -12,3 +12,10 @@
 //     console.log('Rafael is a good name!');
 // }
 
+const continent = 'South America', country = 'Brazil', city = 'Fortaleza', population = 2578412;
+
+const populationFormated = population.toLocaleString('pt-BR');
+console.log(populationFormated);
+
+console.log(`I´m from ${continent} and I live in ${city}, ${country}.`)
+console.log(`The population of ${city} is ${populationFormated}. Currently, ${city} has the fourth largest population in ${country}.`);
