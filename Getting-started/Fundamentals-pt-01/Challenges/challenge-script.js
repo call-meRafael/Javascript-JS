89251cc685dbc835f464bb658f0e129da48c805b
countdown-br <raaphael.araujo@gmail.com>
2025-10-21 13:36:10 -0300

    taking if else conditions

--- Getting-started/Fundamentals-pt-01/Challenges/challenge-script.js
+++ Getting-started/Fundamentals-pt-01/Challenges/challenge-script.js
@@ -6,8 +6,10 @@ const heightJohn = 1.95;
 const BMIMark = Math.round(massMark / heightMark ** 2).toFixed(1);
 const BMIJohn = Math.round(massJohn / (heightJohn * heightJohn)).toFixed(1);
 if (BMIMark > BMIJohn) {
-    console.log(`Mark's BMI ${BMIMark} is higher than John's BMI ${BMIJohn}! congrats, Mark, you are a healthier man! 
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's BMI (${BMIJohn})! congrats, Mark, you are a healthier man! 
     John needs to get in shape!`);
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's BMI (${BMIMark})! Congrats, John, you are in shape!`);
 }
 
 const markHigherBMI = BMIMark > BMIJohn; 
