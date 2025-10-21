0ccc20edddd4e0eb4327971b878d20cb6d481b53
countdown-br <raaphael.araujo@gmail.com>
2025-10-21 11:26:49 -0300

    reaching the second challenge from the remembering studies

---
+++ Getting-started/Fundamentals-pt-01/Challenges/challenge-script.js
@@ -0,0 +1,14 @@
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = Math.round(massMark / heightMark ** 2).toFixed(1);
+const BMIJohn = Math.round(massJohn / (heightJohn * heightJohn)).toFixed(1);
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI ${BMIMark} is higher than John's BMI ${BMIJohn}! congrats, Mark, you are a healthier man! 
+    John needs to get in shape!`);
+}
+
+const markHigherBMI = BMIMark > BMIJohn; 
+console.log(`Mark's BMI ${BMIMark} is higher than John's BMI ${BMIJohn}. ${markHigherBMI ? 'Mark is a healthier man!' : 'John is a healthier man!'}`);
--- Getting-started/Fundamentals-pt-01/script.js
+++ Getting-started/Fundamentals-pt-01/script.js
@@ -12,10 +12,56 @@
 //     console.log('Rafael is a good name!');
 // }
 
-const continent = 'South America', country = 'Brazil', city = 'Fortaleza', population = 2578412;
+// const continent = 'South America', country = 'Brazil', city = 'Fortaleza', population = 2578412;
 
-const populationFormated = population.toLocaleString('pt-BR');
-console.log(populationFormated);
+// const populationFormated = population.toLocaleString('pt-BR');
+// console.log(populationFormated);
 
-console.log(`I´m from ${continent} and I live in ${city}, ${country}.`)
-console.log(`The population of ${city} is ${populationFormated}. Currently, ${city} has the fourth largest population in ${country}.`);
+// console.log(`I´m from ${continent} and I live in ${city}, ${country}.`)
+// console.log(`The population of ${city} is ${populationFormated}. Currently, ${city} has the fourth largest population in ${country}.`);
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
+// console.log(BMIMark, BMIJohn, markHigherBMI);
+
+// const firstName = 'Rafael';
+// const job = 'Full Stack developer';
+// const birthYear = 1992;
+// const year = 2037;
+
+// const rafael = "i'm " + firstName + ', a ' + (year - birthYear) + 'years old ' + job + '!';
+// console.log(rafael)
+
+// const rafaelNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!!!`;
+// console.log(rafaelNew)
+
+// console.log(`String
+//         with
+//         multiple
+//         lines`
+// );
+
+const age = 15;
+
+
+if (age >= 18) {
+    console.log('Sarah can start driving! 🚗')
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah needs to wait ${yearsLeft} more years to start driving!`)
+}
+
+let century;
+const birthYear = 1992;
+if (birthYear <= 2000) {
+    century = '20';
+} else {
+    century = '21';
+}
+console.log(century);
