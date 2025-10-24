40cab10024aefb3b8630c4211cf594515120d885
countdown-br <raaphael.araujo@gmail.com>
2025-10-24 13:38:08 -0300

    going through the basics of functions

---
+++ Fundamentals-part-2/script.js
@@ -0,0 +1,34 @@
+// getting started with Strict Mode
+/* strict mode show us any error in our syntax making our code more secure. It's also preveent us from using undeclared variables and functions. Strict mode also prevent the usage of some variable names which are or will be reserved by the Javascript language.  */
+
+/* 'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive!'); */
+
+
+// Functions
+
+function logger() { // function declaration without parameters
+    console.log('My name is Rafael');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) { // function declaration with parameters(arguments)
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0); /* our variable appleJuice will store the value returned from the function fruitProcessor when we call it with 5 apples and 0 oranges as arguments */
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
