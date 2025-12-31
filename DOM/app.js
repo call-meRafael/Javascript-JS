96e3b614e73d9d18bc0b243ff2c01c51bb9f951d
countdown-br <raaphael.araujo@gmail.com>
2025-12-31 12:27:22 -0300

    find-the-number project: finishing logic and visual concepts

--- DOM/app.js
+++ DOM/app.js
@@ -1,27 +1,81 @@
 
 const mensagem = document.querySelector('.message');
-/*
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 22;
-*/
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-// document.querySelector('.number').textContent = secretNumber;
+let score = 7;
 
 
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+
+// ======= Botão de verificação do jogo ====== //
 document.querySelector('.btn-check').addEventListener('click', function () {
     let userGuess = Number(document.querySelector('.guess').value);
     console.log(userGuess, typeof userGuess);
+    
 
+    // Se não houver número digitado
     if (!userGuess) {
         mensagem.textContent = 'Escolha um número antes de prosseguir!';
+
+        // Se o número digitado for igual ao número secreto
     } else if (userGuess === secretNumber) {
+        document.querySelector('.number').textContent = secretNumber;
         mensagem.textContent = `Você acertou! O número secreto é ${secretNumber}. Parabéns!`;
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.guess').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '12rem'
+
+
+        // Se o número digitado for maior que o número secreto
     } else if (userGuess > secretNumber) {
-        mensagem.textContent = 'Friooo! ❄️ Tente um número menor!'
+
+        // Se as chances forem maiores que 1
+        if (score > 1 ) {
+            mensagem.textContent = 'Friooo! ❄️ Tente um número menor!'
+            score--;
+            document.querySelector('.score').textContent = score;
+
+            // Se as chances acabarem
+        } else {
+            mensagem.textContent = 'Você perdeu! 😩 Tente novamente!'
+            document.querySelector('.score').textContent = 0;
+        }
+
+        // Se o número digitado pelo usuário for menor que o número secreto
     } else if (userGuess < secretNumber) {
-        mensagem.textContent = 'Está ficando quentee! 🔥 Tente um número maior!'
+
+        // Se as chances forem maiores que 1
+        if (score > 1) {
+            mensagem.textContent = 'Está ficando quentee! 🔥 Tente um número maior!'
+            score--;
+            document.querySelector('.score').textContent = score;
+
+            // Se as chances acabarem
+        } else {
+            mensagem.textContent = 'Você perdeu! 😩 Tente novamente!'
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });
+
+
+// ===== Botão de reiniciar o jogo ====== //
+const resetBtn = document.querySelector('.btn-again');
+resetBtn.addEventListener('click', function () {
+    score = 7;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.highscore').textContent = '0';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.guess').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = 'auto';
+    mensagem.textContent = 'Comece a Adivinhar...';
+    
+    
+})
+
+
