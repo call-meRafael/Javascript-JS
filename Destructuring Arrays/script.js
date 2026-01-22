ecdf7b92d0fd07f448dd21c5a62392dc0d7e3fdf
countdown-br <raaphael.araujo@gmail.com>
2026-01-22 12:27:26 -0300

    advancing in desconstructuring

--- Destructuring Arrays/script.js
+++ Destructuring Arrays/script.js
@@ -6,11 +6,78 @@ const restaurant = {
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
+
+
     order: function (starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function ({ 
+        starterIndex,
+        mainIndex,
+        time,
+        address,
+    }) {
+        console.log(
+            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered at ${time} at ${address}.
+            `
+
+        );
     }
 };
 
+restaurant.orderDelivery({
+    time: '22:30',
+    address: 'Via del Sole, 21',
+    mainIndex: 2,
+    starterIndex: 2,
+});
+
+
+
+/*
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+    name: restaurantName,
+    openingHours: hours,
+    categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] }= restaurant;
+console.log(menu, starters);
+
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({a, b} = obj);
+console.log(a, b);
+
+
+// Nested objects
+const { fri: { open, close } } = openingHours
+console.log(open, close);
+*/
+
+/*
 // desstructuring
 const arr = [2, 3, 4];
 const a = arr[0];
@@ -44,3 +111,4 @@ const nested = [2, 4, [5, 6]];
 
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
+*/
