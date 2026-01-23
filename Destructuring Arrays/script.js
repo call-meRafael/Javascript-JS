2f089cadcd72c4823b7f5440c0c6f67928577ed7
countdown-br <raaphael.araujo@gmail.com>
2026-01-23 12:27:31 -0300

    formating doc

--- Destructuring Arrays/script.js
+++ Destructuring Arrays/script.js
@@ -1,53 +1,50 @@
-'use strict';
+"use strict";
 
 const restaurant = {
-    name: 'Classico Italiano',
-    location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-    openingHours: {
-        thu: {
-            open: 12,
-            close: 22,
-        },
-        fri: {
-            open: 11,
-            close: 23,
-        },
-        sat: {
-            open: 0,
-            close: 24,
-        },
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
     },
-
-
-    order: function (starterIndex, mainIndex) {
-        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    fri: {
+      open: 11,
+      close: 23,
     },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
 
-    orderDelivery: function ({ 
-        starterIndex,
-        mainIndex,
-        time,
-        address,
-    }) {
-        console.log(
-            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered at ${time} at ${address}.
-            `
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
-        );
-    }
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered at ${time} at ${address}.
+            `,
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}.`,
+    );
+  },
 };
 
 restaurant.orderDelivery({
-    time: '22:30',
-    address: 'Via del Sole, 21',
-    mainIndex: 2,
-    starterIndex: 2,
+  time: "22:30",
+  address: "Via del Sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
 });
-
-
 
 /*
 const { name, openingHours, categories } = restaurant;
@@ -113,7 +110,6 @@ const [i, , [j, k]] = nested;
 console.log(i, j, k);
 */
 
-
 //========= Spread Operator ==========//
 // Expande um array em todos os seus elementos individualmente
 const arr = [7, 8, 9];
@@ -128,7 +124,7 @@ console.log(optNewArray);
 
 // TODO: O spread operator pode ser utilizado em qualque rlugar onde se espera valores separados por vírgula.
 // TODO: Ele não cria variáveis novas, e também não permite expandir um determinado elemento de um array, apenas o todo.
-const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+const newMenu = [...restaurant.mainMenu, "Gnocci"];
 console.log(newMenu);
 
 const newMenuCopy = [...restaurant.mainMenu];
@@ -139,3 +135,17 @@ const newMenuCopy = [...restaurant.mainMenu];
 ////testando novamente
 const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
 console.log(menu);
+
+////Iterables: arrays, strings, maps, sets, NOT objects
+const str = "Rafael";
+const letters = [...str, " ", "S."];
+console.log(letters);
+
+const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt("Ingredient 2?"),
+//   prompt("Ingredient 3?"),
+];
+console.log(ingredients);
+
+restaurant.orderPasta(...ingredients);
