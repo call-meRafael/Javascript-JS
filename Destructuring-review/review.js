f2991df91abd0094cd42a457f4cb4a31c5fc35d7
countdown-br <raaphael.araujo@gmail.com>
2026-02-19 08:55:28 -0300

    getting started with the review

---
+++ Destructuring-review/review.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+};
+
