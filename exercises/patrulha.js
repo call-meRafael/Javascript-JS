acf6bac0c5d6d22ea2a40e1f0601b1a9a490dc0d
countdown-br <raaphael.araujo@gmail.com>
2026-02-26 08:43:15 -0300

    new exercise

---
+++ exercises/patrulha.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const guardioes = [
+    {
+        nome: ,
+        titulo: ,
+        casa: ,
+        nivel: ,
+        status: {
+            vida: undefined,
+            mana: undefined,
+            defesa: undefined,
+            inventario: ['Espada da Guarda', 'Manto da Guarda', 'Armadura de Couro']
+        }
+    }
+]
+
+function adicionarGuardiao(guardiao, { titulo = 'Patrulheiro', status = { vida: 100, mana: 100, defesa: 75, inventario: ['Espada da Guarda', 'Manto da Guarda', 'Armadura de Couro'] } }) {
+    
+}
