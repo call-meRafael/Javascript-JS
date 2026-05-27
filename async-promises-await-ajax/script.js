abfb0a8f38cdfb72bd2d8adbacd6e60be2aa1a93
countdown-br <raaphael.araujo@gmail.com>
2026-05-27 11:09:41 -0300

    trying to get the neighbour country to show

--- async-promises-await-ajax/script.js
+++ async-promises-await-ajax/script.js
@@ -98,13 +98,43 @@ const request = new XMLHttpRequest();
 request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 request.send();
 */
+const renderCountry = function (data, className = '') {
+    const html = `
+    <article class="country ${className}">
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name.nativeName.por.official}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].symbol} ${data.currencies[0].name}</p>
+        </div>
+    </article>
+    `;
+    countriesCountainer.insertAdjacentHTML('beforeend', html);
+    countriesCountainer.style.opacity = 1;
+}
 
-const request = fetch(`https://restcountries.com/v3.1/name/portugal`);
-console.log(request);
+// const request = fetch(`https://restcountries.com/v3.1/name/portugal`);
+// console.log(request);
 
 const getCountryData = (country) => {
+    // Country 1
   fetch(`https://restcountries.com/v3.1/name/${country}`).then(response => response.json())
-    .then(data => console.log(data))
-  ;
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      if (!neighbour) return;
+      
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'))
 };
-getCountryData('brazil');
+getCountryData('portugal');
+
+
+const dataSup = fetch(`https://restcountries.com/v3.1/name/portugal`).then(response => response.json());
+console.log(dataSup);
