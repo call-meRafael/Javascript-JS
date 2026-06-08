f67a767d3956abdf82eb761fb77406b17e315726
countdown-br <raaphael.araujo@gmail.com>
2026-06-08 11:11:16 -0300

    generates a function that tells if the characters has determined stats and a function that returns a new promise to insert resolve and reject as edge cases;

--- async-promises-await-ajax/rpgmonsters.js
+++ async-promises-await-ajax/rpgmonsters.js
@@ -15,18 +15,17 @@ const monsters = [
     type: "Serpent",
   },
   {
-    name: 'Hoddrik',
+    name: "Hoddrik",
     hp: 35,
-    title: 'The Gray Horror',
-    type: 'Great Orc'
+    title: "The Gray Horror",
+    type: "Great Orc",
   },
   {
-    name: 'Rue',
+    name: "Rue",
     hp: 20,
-    title: 'The Faraway Slime',
-    type: 'Ancestral Slime'
-  }
-
+    title: "The Faraway Slime",
+    type: "Ancestral Slime",
+  },
 ];
 
 // Isolando o fetch dos dados
@@ -34,7 +33,9 @@ fetch("https://api.open5e.com/v1/monsters/")
   .then((response) => {
     // Verifica se a resposta foi bem sucedida (status 200-299), caso não, retorna uma mensagem de erro com status e texto da resposta para melhor identificação do problema. (nota: Teste referente ao DIA 2 de exercícios.)
     if (!response.ok) {
-      throw new Error(`Failed to fetch data: (${response.status}: ${response.statusText})`);
+      throw new Error(
+        `Failed to fetch data: (${response.status}: ${response.statusText})`,
+      );
     }
     return response.json();
   })
@@ -44,33 +45,37 @@ fetch("https://api.open5e.com/v1/monsters/")
     const dragons = data.results.filter((monster) => monster.type === "Dragon");
 
     // Intercalação referente ao DIA 2, onde somamos o total de HP dos inimigos determinados pelo procedimento de filtragem. (Nota: A API retorna o hp como number por padrão, decidi explicitamente converter para Number para garantir a consistência, no mundo real, não podemos confiar que os dados da API estejam sempre no formato esperado.)
-    const totalHitPoints = dragons.reduce((acc, specEnemy) => acc + Number(specEnemy.hit_points), 0);
+    const totalHitPoints = dragons.reduce(
+      (acc, specEnemy) => acc + Number(specEnemy.hit_points),
+      0,
+    );
     console.log(dragons, totalHitPoints);
     console.log("-------------------DIA 2-------------------");
   })
   // Catch básico para lidar com erros de rede ou problemas na resposta da API
   .catch((err) => console.error(err.message));
-;
-
 // Criando um array básico isolado e aplicando desestruturação direta
 const dangerousMonsters = monsters
   .filter(({ hp }) => hp >= 50)
-  .map((monster) => ({ ...monster, dangerous: true }))
-;
+  .map((monster) => ({ ...monster, dangerous: true }));
 console.log(dangerousMonsters);
 console.log(monsters);
-
 
 ////-------------------------------------DIA 2---------------------------------
 
 //Part 1.
 // Build an Array of of items from the hero's inventory, using reduce() sum the weight of all items carried by the hero and after that, using find(), search for the item of the lighest weight and return a formated string of the whole process.
 const inventoryList = [
-    { name: 'Diamond Sword', type: 'Weapon', rarity: 'Legendary', weight: 2.2 },
-    { name: 'Healing Potion', type: 'Consumable', rarity: 'Common', weight: 0.2 },
-    { name: 'Emperor\'s Armor', type: 'Armor', rarity: 'Epic', weight: 12.8 },
-    { name: 'Hermes Boots', type: 'Armor', rarity: 'Divine', weight: 1.7 },
-    { name: "Dragonslayer's Shield", type: 'Armor', rarity: 'Legendary', weight: 5.7 }
+  { name: "Diamond Sword", type: "Weapon", rarity: "Legendary", weight: 2.2 },
+  { name: "Healing Potion", type: "Consumable", rarity: "Common", weight: 0.2 },
+  { name: "Emperor's Armor", type: "Armor", rarity: "Epic", weight: 12.8 },
+  { name: "Hermes Boots", type: "Armor", rarity: "Divine", weight: 1.7 },
+  {
+    name: "Dragonslayer's Shield",
+    type: "Armor",
+    rarity: "Legendary",
+    weight: 5.7,
+  },
 ];
 
 // Aqui, temos o código separado das requisições do exercício, decidi transformá-las em uma única função reutilizável mesmo que não fosse necessário.
@@ -86,9 +91,55 @@ const formatOutput = (data) => {
   return `
     The heroe is carrying ${totalWeight}kg of items and the lightest one is: (${lightestItem.name}) with only ${lightestItem.weight}kg!
     `;
-}
+};
 console.log(formatOutput(inventoryList));
 
+// ---------------------------------------DIA 3---------------------------------
+// Part 1.
+// Build an Array of character's stats, use .some() to verify if the character has any value less than 10 (return true or false). Using .every() check if all stats are greater than 8.
+
+const defineStats = (str, dex, con, int, wis, cha) => {
+  return {
+    strength: str,
+    dexterity: dex,
+    constitution: con,
+    intelligence: int,
+    wisdom: wis,
+    charisma: cha,
+  }
+};
+
+const heroStats = [
+  {
+    stats: defineStats(12, 8, 15, 9, 14, 11)
+  }
+];
 
 
+const checkStats = heroStats.map(({ stats }) => {
+  const lowStats = Object.keys(stats).some((key) => stats[key] < 10);
+  const highStats = Object.keys(stats).every((key) => stats[key] >= 8);
+  
+  return  { lowStats, highStats };
+});
+
+const heroActions = () => {
+  
+  return new Promise((resolve, reject) => {
+    const rollTheDice = Math.trunc(Math.random() * 20 + 1); // Simulates a D20 roll
+    console.log(`Rolling the dice: ${rollTheDice}`);
+
+    if (rollTheDice >= 16) {
+      
+      resolve("Critical hit!");
+    } else if (rollTheDice <= 3) {
+      reject(new Error("You failed to hit!"));
+    } else {
+      resolve("You hit!");
+    }
+  });
+
+};
+heroActions().then((res => console.log(res))).catch(err => console.log(err));
+console.log(checkStats);
 
